ad75dfbd5c81ed7dea88f435f78ef39681e51871
Chupii <162031546+Chupii37@users.noreply.github.com>
2025-01-06 19:42:03 +0700

    hyperspace-node.sh

--- hyperspace-node.sh
+++ hyperspace-node.sh
@@ -55,7 +55,7 @@ check_and_install_docker() {
   if ! command -v docker &> /dev/null; then
     echo -e "${RED}Docker tidak ditemukan. Menginstal Docker...${NC}"
     apt-get install -y apt-transport-https ca-certificates curl software-properties-common
-    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - 
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
     add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
     apt update -y
     apt install -y docker-ce
